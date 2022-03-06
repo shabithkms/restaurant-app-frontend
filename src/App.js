310c43f756ecab8ae5748f2c0c8721058a8b9a2e
shabithkms <shabithkms2035@gmail.com>
2022-03-06 13:25:33 +0530

    started modifier management

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
 import { Toaster } from 'react-hot-toast';
-import {Routes,Route} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom';
 import AdminLogin from './Pages/AdminLogin';
 import HomePage from './Pages/AdminHome';
+import EditItem from './Pages/EditItem';
 import AddCategory from './Pages/Categories';
+import Modifiers from './Pages/Modifiers';
 
 function App() {
-  return <div className='App'>
-
-     {/* React hot toast */}
-     <Toaster
+  return (
+    <div className='App'>
+      {/* React hot toast */}
+      <Toaster
         position='top-center'
         reverseOrder={false}
         gutter={8}
@@ -33,12 +35,15 @@ function App() {
           },
         }}
       />
-    <Routes>
-      <Route exact path='/' element={<HomePage/>}/>
-      <Route path='/login' element={<AdminLogin/>}/>
-      <Route path='/categories' element={<AddCategory/>}/>
-    </Routes>
-  </div>;
+      <Routes>
+        <Route exact path='/' element={<HomePage />} />
+        <Route exact path='/edit-item/:id' element={<EditItem />} />
+        <Route path='/login' element={<AdminLogin />} />
+        <Route path='/categories' element={<AddCategory />} />
+        <Route path='/modifier' element={<Modifiers />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
